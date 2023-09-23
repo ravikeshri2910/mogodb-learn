62100cd2f8c7dc70aa3586f9ca7cf7d4cdb3f5ec
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-09-23 15:39:39 +0530

    Integrating CRUD with MongoDB

--- models/product.js
+++ models/product.js
@@ -7,22 +7,22 @@ class Product {
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    // this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id): null
   }
 
   save() {
     const db = getDb();
-    // let dbOp;
-    // if (this._id) {
-    //   // Update the product
-    //   dbOp = db
-    //     .collection('products')
-    //     .updateOne({ _id: this._id }, { $set: this });
-    // } else {
-    //   dbOp = db.collection('products').insertOne(this);
-    // }
-    // return dbOp
-    return db.collection('products').insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection('products').insertOne(this);
+    }
+    return dbOp
+    // return db.collection('products').insertOne(this)
       .then(result => {
         console.log(result);
       })
@@ -50,7 +50,7 @@ class Product {
     const db = getDb();
     return db
       .collection('products')
-      .find({ _id: new mongodb.ObjectId(prodId) })
+      .find({ _id: new mongodb.ObjectId(prodId) }) // extract id from mogodb.ObjectId
       .next()
       .then(product => {
         console.log(product);
