a48f0a1787331345fef492ea580756b6710f5cb4
ravikeshri2910 <ravikeshri2910@gmail.com>
2023-09-25 12:21:09 +0530

    Add cart

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -13,19 +14,17 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const User = require('./models/user')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findUserById('650ec1575f1667970a1cce9b')
+  User.findById('650ec1575f1667970a1cce9b')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
- 
 });
 
 app.use('/admin', adminRoutes);
